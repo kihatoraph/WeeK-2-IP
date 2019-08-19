59a50dc87fdd928fab839c942caf23607deea860
Raphael Kihato <kihatoraph@gmail.com>
2019-08-19 03:51:02 -0400

    Added js function

---
+++ script.js
@@ -0,0 +1,8 @@
+var century, month, dayOfWeek, dayOfMonth, day;
+// get input
+function getInput(){
+    century = parseInt(document.getElementById("Century").value);
+    year = parseInt(document.getElementById)("Year").value);
+    month  = parseInt(document.getElementById)("Month").value;
+    day = parseInt(document.getElementById)("Day").value;
+}
