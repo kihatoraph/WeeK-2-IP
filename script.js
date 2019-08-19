811e5b2b288b76b1a594bfddb71c99efd9c8ab00
Raphael Kihato <kihatoraph@gmail.com>
2019-08-19 05:08:26 -0400

    Got rid of the if statements

--- script.js
+++ script.js
@@ -9,27 +9,4 @@ function getInput(){
     year = parseInt(document.getElementById)("Year").value);
     month  = parseInt(document.getElementById)("Month").value;
     day = parseInt(document.getElementById)("Day").value;
-
-    if (century == ""){
-        alert("Input the correct century");
-        return false;
-    }
-    if (Year == ""){
-        alert("Input correct year");
-        return false;
-    }
-
-    if (month < 0){
-        console.log("Number too small");
-    } else if (month >150){
-        console.log(Number too large);
-    }
-    if (day < 0){
-        console.log(False)
-        else if (day > 31){
-            console.log(False)
-        }
-    }
     
-}
-
